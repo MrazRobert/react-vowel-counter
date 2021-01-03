b23e559bfdc89d56bbf9ed01ad838bd66010a086
Mraz Robert <bob.mr85@gmail.com>
2021-01-04 00:03:25 +0100

    focus added to textarea

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
 
 function App() {
+  const [item, setItem] = useState('');
+  const inputField = useRef('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!item) {
+      alert("Please enter value!");
+    }else {
+      alert(`There are ${countVowel(item)} vowels!`);
+    }
+  }
+
+  useEffect(() => {
+    inputField.current.focus();
+  }, []);
+
+  const countVowel = (item) => {
+    const regex = /[a,á,e,é,i,í,o,ó,ö,ő,u,ú,ü,ű,A,Á,E,É,I,Í,O,Ó,Ö,Ő,U,Ú,Ü,Ű]/g;
+    if(!item.match(regex)) {
+      return 0;
+    }else {
+      return item.match(regex).length;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Vowel Counter</h1>
+      <form className="form">
+        <textarea 
+          name="area" 
+          id="area" 
+          cols="100" 
+          rows="3"
+          value={item}
+          ref={inputField}
+          onChange={(e) => setItem(e.target.value)}
+        ></textarea>
+        <div className="button-container">
+          <button type="submit" className="btn" onClick={handleSubmit}>Check</button>
+        </div>
+      </form>
+    </section>
   );
 }
 
